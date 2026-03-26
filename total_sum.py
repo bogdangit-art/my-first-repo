3cff43b818bac0bb0dfa2ad81232c64d1e4ede0a
DRAGOMIR BOGDAN <dragobogdan123@gmail.com>
2026-03-26 13:34:46 +0200

    Made a total_sum function

--- total_sum.py
+++ total_sum.py
@@ -1,1 +1,24 @@
 # this file calculate total sum of all products
+
+def calculate_total():
+    prices = []
+    print("Enter price: (end for stop)")
+
+    while True:
+        user_input = input("Price: ")
+        if user_input.lower() == 'end':
+            break
+        try:
+            price = float(user_input)
+            prices.append(price)
+        except ValueError:
+            print("Enter number or END for stop")
+
+    if prices:
+        total_price = sum(prices)
+        print(f"Total sum is: {total_price:.2f}")
+    else:
+        print("You not enter any price.")
+
+calculate_total()
+
